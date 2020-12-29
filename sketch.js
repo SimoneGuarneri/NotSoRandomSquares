73736dfffcab5eb4cb70de11e22c12d5c2c28fd2
Simone Guarneri <47464913+SimoneGuarneri@users.noreply.github.com>
2020-12-29 15:27:10 +0100

    Added some randomness

--- sketch.js
+++ sketch.js
@@ -1,53 +1,61 @@
-let MAXITER = 30;
+let MAXITER;
 let iteration = 0;
 
 let colors = [];
 let rects = [];
 
 function setup() {
+
+    MAXITER = int(random(5, 40));
+
     generateColors();
     createCanvas(windowWidth, windowHeight);
+
     rectMode(CENTER);
     stroke(colors[0]);
     background(colors[0]);
     strokeWeight(2);
-
+    
+    //TEXT
     fill(colors[2]);
     textAlign(CENTER);
     textFont('Lato');
     textWidth(700);
     textSize(15);
-    text("simoneguarneri.github.io/notsorandomsquare", windowWidth/2, windowHeight-10);
-
+    text("simoneguarneri.github.io/notsorandomsquares", windowWidth/2, windowHeight-10);
 }
 
 function draw(){
 
-  
   fill(colors[int(random(1,5))]);
+  
   let coords;
   do{
       coords = {
         x: int(random(windowWidth/2-200, windowWidth/2+200)),
-        y: int(random(100, 400)),
+        y: int(random(windowHeight/2-150, windowHeight/2+100)),
         w: int(random(50, 200)),
         h: int(random(50, 200)),
       };
     
   }while(isNear(coords));
+
   rects.push(coords);
   rect(coords.x, coords.y, coords.w, coords.h, 5);
   
   iteration++;
   if(iteration >= MAXITER)
       noLoop();
+
 }
 
 function isNear(coord){
   for(let i = 0; i < rects.length; i++){
     let checking = rects[i];
+
     if(dist(coord.x, coord.y, checking.x, checking.y) < coord.w/2)
       return true;
+
     if(dist(coord.x, coord.y, checking.x, checking.y) < coord.h/2)
       return true;
   }
